2a4c96e81b79fbe8ddc7996e678e526e1c4fc8de
Your Name <you@example.com>
2023-10-15 20:49:36 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,6 @@ module "s3-bucket" {
   source  = "app.terraform.io/debobhad-org-aws/s3-bucket/aws"
   version = "2.8.0"
   bucket_prefix = "nrajb"
-  acl    = "log-delivery-write"
 
   # Allow deletion of non-empty bucket
   force_destroy = true
